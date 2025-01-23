8820dc997b95264d4824aa9c6f926ee700346f82
kalumbamon <hsbabdulkadil22@src.pshs.edu.ph>
2025-01-23 08:55:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let surfaceareaofasphere = 4*pi*r**2;
 let volumeofasphere = 4/3*pi*r**3;
 
 document.write("The radius of the sphere is " + r + ".");
+document.write('<br><br>');
 document.write("The circumference is " +  circumferenceofasphere + ".");
-document.write("The surface area is " + surfaceareaofasphere + "." );
+document.write('<br><br>');
+document.write("The surface area is " + surfaceareaofasphere + ".");
+document.write('<br><br>');
 document.write("The volume is " + volumeofasphere + ".");
+ 
